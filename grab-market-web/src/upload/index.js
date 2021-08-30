704a5306bea7f49d662ae3dea72bec7bfcc22b33
chaeshee0908 <chaeshee0908@naver.com>
2021-08-31 01:15:41 +0900

    8/31

--- grab-market-web/src/upload/index.js
+++ grab-market-web/src/upload/index.js
@@ -1,19 +1,47 @@
-import {Form, Divider, Input, InputNumber, Button} from "antd";
+import {Form, Divider, Input, InputNumber, Button, Upload} from "antd";
 import './index.css';
+import {ForkOutlined} from "@ant-design/icons";
+import { useState } from 'react';
 
 function UploadPage(){
+    const [imageUrl, setImageUrl] = useState(null);
     // lambda함수 적용
     const onSubmit = (values) => {
         console.log(values);
+    };
+    const onChangeImage = (info) => {
+        // uploading : 파일을 업로드해서 네트워크 요청이 끝날 때 까지의 과정 => 별다른 처리를 해주지 않음
+        if (info.file.status === 'uploading'){
+            return;
+        }
+        // done : 완료가 되면 
+        if (info.file.status === 'done'){
+            const response = info.file.response;
+            const imageUrl = response.imageUrl;
+            setImageUrl(imageUrl);
+        }
     }
     return (
         <div id="upload-container">
             <Form name="상품 업로드" onFinish={onSubmit}>
                 <Form.Item name="upload" label={<div className="upload-label">상품 사진</div>}>
-                    <div id="upload-img-placeholder">
-                        <img src="/images/icons/camera.png" />
-                        <span>이미지를 업로드해주세요.</span>
-                    </div>
+                    <Upload 
+                        name="image" 
+                        action="http://localhost:8080/image" 
+                        listType="picture"
+                        showUploadList={false}
+                        onChange={onChangeImage}
+                    >
+                        {imageUrl ? (
+                            <img id="upload-img" src={`http://localhost:8080/${imageUrl}`} /> 
+                            ) : (
+                                <div id="upload-img-placeholder">
+                                    <img src="/images/icons/camera.png" />
+                                    <span>이미지를 업로드해주세요.</span>
+                                </div>
+                            )
+                        }
+                    </Upload>
                 </Form.Item>
                 {/* 선으로 나눠주는 용도 */}
                 <Divider />
